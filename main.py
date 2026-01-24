1172a8761796f553f56fc687b607a37b50b3db41
Leo Moreno <leo.moreno@pm.me>
2026-01-24 13:28:40 -0500

    add .env dotenv

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from src import handlers
 import os
 import telebot
 import logging
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Configuración básica
 logging.basicConfig(
@@ -27,14 +30,14 @@ def start_bot():
 
 def main():
     logging.info("El programa ha iniciado")
-    bot = start_bot()
-    handlers.register_handlers(bot)
-    bot.infinity_polling(
-        restart_on_change=False,
-        timeout=10,
-        logger_level=logging.ERROR
-    )
-    # print(TOKEN)
+    # bot = start_bot()
+    # handlers.register_handlers(bot)
+    # bot.infinity_polling(
+    #     restart_on_change=False,
+    #     timeout=10,
+    #     logger_level=logging.ERROR
+    # )
+    print(TOKEN)
 
 if __name__ == "__main__":
     main()
